b0c1e8ea3650cfdbbd452eb355c7d2b1f619a04d
Aubrey Ford <musicaubrey@gmail.com>
2015-12-10 12:29:05 -0700

    only post left

--- public/playlist.js
+++ public/playlist.js
@@ -16,19 +16,62 @@ $(function(){
     method: "POST"
 
   })
-  getter.done(function(response){
-    console.log(response)
-  })
-  poster.done(function(response){
-    console.log(response)
-  })
+
+
+  function printAlbum (idNumber, array){
+        var output = null
+        array.forEach(function(e){
+            if (idNumber == e['id']){
+              output = e['artist']+ ": " + e["title"]
+            }
+        })
+        return output
+  }
 /////////////////////
 
   getter.done(function(response){
+
+
     response.results.forEach(function(e){
-        $(".overwide").append("<img class='album' src='images/e['cover_art']'")
+        $(".overwide").append("<img name='" + e['title'] + "' id='" + e['id'] + "' class='album' src='images/" + e['cover_art'] + "'>")
     })
+
+//////////////////
+
+    $(".album").on("click", function(){
+
+        var clickedId = $(this).attr("id")
+        var nameVal = $(this).attr("name")
+
+        var info = printAlbum(clickedId, response['results'])
+
+        $("select").append("<option value='"+nameVal+"'>"+info+"</option>")
+
+
+    })
+
+    $(".clear").on("click", function(){
+        $("select").children().remove()
+    })
+
+    $(".submitBin").on("click", function(){
+
+
+
+      var names = []
+      $("select").children().each(function(a,e){
+            names.push($(this).val())
+      })
+      console.log(names)
+
+    })
+
+
+
+
   })
+
+
 
 
 
--- public/splash.js
+++ public/splash.js
@@ -8,9 +8,32 @@ $(function(){
 
     })
 
+    function randomNums(){
+      var arr = [0,1,2,3,4]
+      var output = []
+      var count = 5
+      for (var i = 0; i < 3; i++) {
+        var a = Math.floor(Math.random() * count)
+        var b = arr.splice(a, 1)
+        output.push(b[0])
+        count--
+      }
+     return output
+    }
+
+
+
     getter.done(function(response){
-      console.log(response)
+              var randy = randomNums()
+        for (var i = 0; i < 3; i++) {
+
+              $(".right").append("<img class='randomImg center' src='images/"+response['results'][randy.pop()]['cover_art']+"'>")
+        }
     })
+
+    
+
+
 
 
 
